3e8388c62e19e1fcbdfbfd961937b9a16dfa1516
Devin Ludwig <devinludwig@gmail.com>
2017-02-23 19:50:13 -0800

    add migration for 'in progress' boolean column in jobs; add materialize; add non-functioning ajax to move 'in-progress' jobs to separate list in worker show path

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -25,15 +25,28 @@ class JobsController < ApplicationController
   def update
     @job = Job.find(params[:id])
     if current_worker
-      if @job.update(pending: true, worker_id: current_worker.id)
-        respond_to do |format|
-          format.html { redirect_to worker_path(current_worker) }
-          format.js
+      if params[:change] == 'progress'
+        if @job.update(in_progress: true)
+          respond_to do |format|
+            format.html { redirect_to worker_path(current_worker) }
+            format.js
+          end
+          flash[:notice] = "You've successfully marked job 'in progress'!."
+        else
+          redirect_to worker_path(current_worker)
+          flash[:notice] = "Something went wrong!"
         end
-        flash[:notice] = "You've successfully claimed this job."
       else
-        render :show
-        flash[:notice] = "Something went wrong!"
+        if @job.update(pending: true, worker_id: current_worker.id)
+          respond_to do |format|
+            format.html { redirect_to worker_path(current_worker) }
+            format.js
+          end
+          flash[:notice] = "You've successfully claimed this job."
+        else
+          render :show
+          flash[:notice] = "Something went wrong!"
+        end
       end
     else
       # We need to streamline this process better in the future! - Mr. Fix-It.
---
+++ db/migrate/20170224033556_add_progress_status_column_to_jobs.rb
@@ -0,0 +1,5 @@
+class AddProgressStatusColumnToJobs < ActiveRecord::Migration[5.0]
+  def change
+    add_column :jobs, :in_progress, :boolean, :default => false
+  end
+end
